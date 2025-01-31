913e4dbaf51bcb9e85662b4c30310826aabf45c8
M1chael Jackson <145285648+M1chaelJackson@users.noreply.github.com>
2025-01-31 01:47:30 +0100

    Update main.cpp
    Adapted code for multi-monitor support

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
-#include "KonataDancer.h"
 #include <QtWidgets/QApplication>
 #include <qrect.h>
 #include "render.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    // starting gif/animation
     QString gifPath = ":/KonataDancer/resource/konata/Konata.gif";
+
+    // initialize window
     CharacterWindow window(gifPath);
     window.show();
 
-    window.move(0, 0);
-    window.setFixedSize(2560, 1440);
+    // calculate the center of the monitor
+    QScreen* screen = QGuiApplication::primaryScreen();
+
+    if (screen) {
+
+        QRect screenGeometry = screen->geometry();
+
+        int screenWidth = screenGeometry.width() / 2 - window.width() / 2;
+        int screenHeight = screenGeometry.height() / 2 - window.height() / 2;
+
+        window.move(screenWidth, screenHeight);
+    }
+    else
+    {
+        // set to 0, 0 if no primary monitor is found
+        window.move(0, 0);
+
+    }
+
+    // set window size to GIF's size
+    window.setFixedSize(window.size());
+
     return app.exec();
 }
